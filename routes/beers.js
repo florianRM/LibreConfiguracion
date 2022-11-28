975ee04a7d0f339de4d524d07c8b06483ef7066e
florianRM <florian.ristea10@gmail.com>
2022-11-28 19:03:23 +0100

    Comprobaciones en cervezas

--- routes/beers.js
+++ routes/beers.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const {getBeers ,getBeer ,addBeer, updateBeer, deleteBeer} = require('../controllers/beers.js');
+const { check } = require('express-validator');
+const { validateFields } = require('../helpers/validate-fields');
+const { existsId } = require('../helpers/db-validators')
 
 router.get('/', getBeers);
 
-router.get('/:id', getBeer);
+router.get('/:id', [
+    check('id', 'No es un id correcto').isMongoId(),
+    check('id').custom(existsId),
+    validateFields
+], getBeer);
 
-router.post('/add', addBeer);
+router.post('/add', [
+    check('Proof', 'Proof cannot be negative').isInt({min: 0}),
+    validateFields
+], addBeer);
 
 router.put('/:id', updateBeer);
 
